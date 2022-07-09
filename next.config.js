53da32b3102a86bc5d3e9e0f7cc34026e7d1bbda
Sahul Kumar Parida <71061738+sahuljr7@users.noreply.github.com>
2022-07-09 22:15:54 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com',
+      'avatars.githubusercontent.com',
+    ],
+  },
+};
+
+module.exports = nextConfig;
